75ae6e25d340fff2e793cf4bb5e3b79496bd3332
Czpyioe <czpyioe@gmail.com>
2025-12-16 14:11:35 +0100

    implement lama rpc urls

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,9 +1,17 @@
 mod rpc;
+use anyhow::Result;
 
 #[tokio::main]
-async fn main() {
-    let tmp = rpc::check_rpc_health().await;
-    for (a,b,c) in tmp{
-        println!("{a}, {b}, {c}");
-    }
+async fn main() -> Result<()> {
+    // let tmp = rpc::check_rpcs_health().await;
+    // for (a,b,c) in tmp{
+    //     println!("{a}, {b}, {c}");
+    // }
+    let urls = rpc::request_lama_rpcs().await?;
+    // for url in urls{
+    //     println!("{}",url);
+    // }
+    println!("{}",urls.len());
+    Ok(())
 }
+
--- bot/src/rpc/rpc.rs
+++ bot/src/rpc/rpc.rs
@@ -2,6 +2,24 @@ use alloy::providers::{Provider, ProviderBuilder};
 use dotenv::dotenv;
 use std::{env, time};
 use std::time::{Duration, Instant};
+// use tokio;
+use anyhow::Result;
+use serde::Deserialize;
+
+
+#[derive(Debug, Deserialize)]
+pub struct Chain {
+    name: String,
+    chain:String,
+    rpc: Vec<RpcUrl>,
+}
+
+
+#[derive(Debug,Deserialize)]
+pub struct RpcUrl{
+    url:String
+}
+
 
 
 pub fn load_rpc_url() -> Vec<String>{
@@ -10,25 +28,41 @@ pub fn load_rpc_url() -> Vec<String>{
     rpcs_urls
 }
 
-pub async fn check_rpc_health()-> Vec<(String,f32,bool)>{
+pub async fn request_lama_rpcs()->Result<Vec<String>>{
+    let url = "https://chainlist.org/rpcs.json";
+    let body:Vec<Chain> = reqwest::get(url)
+        .await?
+        .json()
+        .await?;
+
+    let eth_urls = body.into_iter().filter(|i| i.chain=="ETH").flat_map(|c| c.rpc).map(|rpc| rpc.url).collect();
+    Ok(eth_urls)
+}
+
+pub async fn check_rpcs_health()-> Vec<(String,f32,bool)>{
     let rpcs_urls = load_rpc_url();
     let mut rpcs_health : Vec<(String,f32,bool)> = vec![];
     for url in rpcs_urls{
-        let start = Instant::now();
-        let mut rpc_works = true;
-        match ProviderBuilder::new().connect(&url).await{
-            Ok(v)=>{
-                if let Err(_) = v.get_block_number().await {
-                    rpc_works = false;
-                }
-            },
-            Err(_)=>{
-                rpc_works=false
-            }
-        };
-        let duration = start.elapsed();
-        rpcs_health.push((url,duration.as_secs_f32(),rpc_works));
+        rpcs_health.push(check_one_rpc(url).await);
     }
     rpcs_health
 }
 
+
+
+async fn check_one_rpc(url:String)->(String,f32,bool){
+    let start = Instant::now();
+    let mut rpc_works = true;
+    match ProviderBuilder::new().connect(&url).await{
+        Ok(v)=>{
+            if let Err(_) = v.get_chain_id().await {
+                rpc_works = false;
+            }
+        },
+        Err(_)=>{
+            rpc_works=false
+        }
+    };
+    let duration = start.elapsed();
+    (url,duration.as_secs_f32(),rpc_works)
+}
