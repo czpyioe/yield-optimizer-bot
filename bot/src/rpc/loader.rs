ac290943d03ca405fb34173fe0173f1ffb6b07f3
Czpyioe <czpyioe@gmail.com>
2025-12-20 14:52:20 +0100

    1)rpc: implement get provider method in the manager and add loader functions

--- bot/src/rpc/loader.rs
+++ bot/src/rpc/loader.rs
@@ -1,50 +1,50 @@
-// use alloy::providers::{Provider, ProviderBuilder};
-// use dotenv::dotenv;
-// use std::{env, time};
-// use std::time::{Duration, Instant};
-// // use tokio;
-// use anyhow::Result;
-// use serde::Deserialize;
+use alloy::providers::{Provider, ProviderBuilder};
+use dotenv::dotenv;
+use std::{env, time};
+use std::time::{Duration, Instant};
+// use tokio;
+use anyhow::Result;
+use serde::Deserialize;
 
 
-// #[derive(Debug, Deserialize)]
-// pub struct Chain {
-//     chain:String,
-//     rpc: Vec<RpcUrl>,
-// }
+#[derive(Debug, Deserialize)]
+pub struct Chain {
+    chain:String,
+    rpc: Vec<RpcUrl>,
+}
 
 
-// #[derive(Debug,Deserialize)]
-// pub struct RpcUrl{
-//     url:String
-// }
+#[derive(Debug,Deserialize)]
+pub struct RpcUrl{
+    url:String
+}
 
-// // should load .env rpc and lama rpc form the api
-// async fn load_rpcs_url() -> Result<Vec<String>>{
-//     let mut all_rpcs:Vec<String> = Vec::new();
-//     if let Ok(mut v) = load_env_rpcs(){
-//         all_rpcs.append(&mut v);
-//     }
-//     if let Ok(mut v) = request_lama_rpcs().await{
-//         all_rpcs.append(&mut v);
-//     }
-//     Ok(all_rpcs)
-// }
+// should load .env rpc and lama rpc form the api
+pub async fn load_rpcs_url() -> Result<Vec<String>>{
+    let mut all_rpcs:Vec<String> = Vec::new();
+    if let Ok(mut v) = load_env_rpcs(){
+        all_rpcs.append(&mut v);
+    }
+    if let Ok(mut v) = request_lama_rpcs().await{
+        all_rpcs.append(&mut v);
+    }
+    Ok(all_rpcs)
+}
 
 
-// pub fn load_env_rpcs() -> Result<Vec<String>>{
-//     dotenv().ok();
-//     let rpcs_urls: Vec<String> = env::vars().filter(|(i,_)| i.contains("RPC_URL")).map(|(_,v)| v).collect();
-//     Ok(rpcs_urls)
-// }
+fn load_env_rpcs() -> Result<Vec<String>>{
+    dotenv().ok();
+    let rpcs_urls: Vec<String> = env::vars().filter(|(i,_)| i.contains("RPC_URL")).map(|(_,v)| v).collect();
+    Ok(rpcs_urls)
+}
 
-// pub async fn request_lama_rpcs()->Result<Vec<String>>{
-//     let url = "https://chainlist.org/rpcs.json";
-//     let body:Vec<Chain> = reqwest::get(url)
-//         .await?
-//         .json()
-//         .await?;
+async fn request_lama_rpcs()->Result<Vec<String>>{
+    let url = "https://chainlist.org/rpcs.json";
+    let body:Vec<Chain> = reqwest::get(url)
+        .await?
+        .json()
+        .await?;
 
-//     let eth_urls = body.into_iter().filter(|i| i.chain=="ETH").flat_map(|c| c.rpc).map(|rpc| rpc.url).collect();
-//     Ok(eth_urls)
-// }
+    let eth_urls = body.into_iter().filter(|i| i.chain=="ETH").flat_map(|c| c.rpc).map(|rpc| rpc.url).collect();
+    Ok(eth_urls)
+}
--- bot/src/rpc/manager.rs
+++ bot/src/rpc/manager.rs
@@ -1,4 +1,8 @@
 use std::time::{Duration, Instant};
+use anyhow::Result;
+
+use crate::rpc::loader;
+use crate::rpc::health;
 
 pub struct RpcManager{
     rpc_url: Vec<RpcEndpoint>,
@@ -19,7 +23,7 @@ pub struct RpcEndpoint{
 
 
 impl RpcManager{
-    pub fn new(health_check_interval:Duration,rpc_fetch_interval:Duration) -> Self{
+    pub fn new(health_check_interval:Duration,rpc_fetch_interval:Duration) -> Self {
         Self { 
             rpc_url: Vec::new(), 
             last_health_check: Instant::now(), 
@@ -27,5 +31,30 @@ impl RpcManager{
             health_check_interval,
             rpc_fetch_interval, 
         }
+    }
+
+    async fn get_provider(&mut self)-> Result<()>{
+        let rpcs_url = loader::load_rpcs_url().await?;
+        let mut rpcs_endpoints:Vec<RpcEndpoint> = rpcs_url.iter()
+            .map(|url| RpcEndpoint{
+            url:url.clone(),
+            latency:None,
+            is_healthy:true,
+            last_checked:Instant::now(),
+            use_count:0,
+            last_used:Instant::now()
+        }).collect();
+        self.rpc_url.append(&mut rpcs_endpoints);
+        self.last_rpc_fetch = Instant::now();
+        Ok(())
+    }
+
+    async fn refresh_health() -> Result<()>{
+        Ok(())
+    }
+
+    async fn fetch_new_rpcs(& self)-> Result<()>{
+        let new_url = loader::load_rpcs_url().await?;
+        Ok(())
     }
 }
