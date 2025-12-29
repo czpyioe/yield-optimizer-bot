0e5ddc95e27983065b846f0cdb0fdcc912513feb
Czpyioe <czpyioe@gmail.com>
2025-12-29 15:43:18 +0100

    2)contract bindings: change asset enum logic, merge all protocols assets into on asset enum

--- bot/src/contracts/addresses.rs
+++ bot/src/contracts/addresses.rs
@@ -1,37 +1,99 @@
 use alloy::primitives::Address;
 use anyhow::Result;
 
+
+
 #[derive(Debug, Clone, Copy)]
-pub enum AaveContract{
-    AaveProtocolDataProvider,
-    Pool
+pub enum Asset {
+    USDC,
+    WETH,
 }
 
-pub enum CompoundContract{
-    cTokenv3,
+impl Asset {
+    pub fn all()->Vec<Asset> {
+        vec![
+            Asset::USDC,
+            Asset::WETH
+        ]
+    }
+
+    pub fn name(&self) -> &str{
+        match self {
+            Asset::USDC=>"USDC",
+            Asset::WETH=>"WETH"
+        }
+    }
+
+    pub fn is_on_aave(&self) -> bool {
+        matches!(self,Asset::USDC | Asset::WETH)
+    }
+
+    pub fn is_on_compound(&self) -> bool {
+        matches!(self,Asset::USDC | Asset::WETH)
+    }
 }
+
+
+pub enum Protocol{
+    Aave,
+    Compound
+}
+
+impl Protocol{
+    pub fn all() -> Vec<Protocol> {
+        vec![
+            Protocol::Aave,
+            Protocol::Compound
+        ]
+    }
+
+    pub fn name(&self)-> &str{
+        match self {
+            Protocol::Aave=>"Aave",
+            Protocol::Compound=>"Compound"
+        }
+    }
+
+    pub fn supported_assets(&self) -> Vec<Asset> {
+        match self {
+            Protocol::Aave => vec![Asset::USDC, Asset::WETH],
+            Protocol::Compound => vec![Asset::USDC, Asset::WETH],
+        }
+    }
+    
+    pub fn supports(&self, asset: Asset) -> bool {
+        match self {
+            Protocol::Aave => asset.is_on_aave(),
+            Protocol::Compound => asset.is_on_compound(),
+        }
+    }
+
+}
+
 
 #[derive(Debug, Clone, Copy)]
 pub enum Network{
     Ethereum,
     Arbitrum,
-    Base,
-}
-
-pub enum AaveAsset {
-    USDC,
-    WETH,
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum CompoundAsset {
-    USDC,
-    WETH
 }
 
 impl Network{
+    pub fn name(&self)->&str{
+        match self{
+            Network::Ethereum=>"Ethereum",
+            Network::Arbitrum=>"Arbitrum",
+        }
+    }
+
+    pub fn all() -> Vec<Network> {
+        vec![
+            Network::Ethereum,
+            Network::Arbitrum,
+        ]
+    }
+
     // Aave
-    pub fn get_aave_contract_address(self:&Network,contract: AaveContract) -> Result<Address> {
+    pub fn get_aave_contract_address(&self,contract: AaveContract) -> Result<Address> {
         let addr = match (self, contract) {
             // Ethereum mainnet
             (Network::Ethereum, AaveContract::AaveProtocolDataProvider) => "0x0a16f2FCC0D44FaE41cc54e079281D84A363bECD",
@@ -45,14 +107,14 @@ impl Network{
         };
         Ok(addr.parse()?)
     }
-    pub fn get_asset_address_aave(self:&Network, asset:AaveAsset)->Result<Address>{
+    pub fn get_asset_address_aave(&self, asset:Asset)->Result<Address>{
         let addr = match (self,asset) {
             // Ethereum mainet
-            (Network::Ethereum,AaveAsset::USDC)=>"0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+            (Network::Ethereum,Asset::USDC)=>"0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
 
 
             // Arbitrum mainnet
-            (Network::Arbitrum,AaveAsset::USDC)=>"0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
+            (Network::Arbitrum,Asset::USDC)=>"0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
 
             _=>anyhow::bail!("Asset address not configured yet")
         };
@@ -62,15 +124,15 @@ impl Network{
 
 
     //Compound
-    pub fn get_compound_contract_address(self:&Network,contract: CompoundContract, asset:CompoundAsset) -> Result<Address> {
+    pub fn get_compound_contract_address(&self,contract: CompoundContract, asset:Asset) -> Result<Address> {
         let addr = match (self, contract, asset) {
             // Ethereum mainnet
-            (Network::Ethereum, CompoundContract::cTokenv3, CompoundAsset::USDC) => "0xc3d688B66703497DAA19211EEdff47f25384cdc3",
-            (Network::Ethereum, CompoundContract::cTokenv3, CompoundAsset::WETH) => "0xA17581A9E3356d9A858b789D68B4d866e593aE94",
+            (Network::Ethereum, CompoundContract::CTokenv3, Asset::USDC) => "0xc3d688B66703497DAA19211EEdff47f25384cdc3",
+            (Network::Ethereum, CompoundContract::CTokenv3, Asset::WETH) => "0xA17581A9E3356d9A858b789D68B4d866e593aE94",
 
             // Arbitrum mainnet
-            (Network::Arbitrum, CompoundContract::cTokenv3, CompoundAsset::USDC) => "0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf",
-            (Network::Arbitrum, CompoundContract::cTokenv3, CompoundAsset::WETH) => "0x6f7D514bbD4aFf3BcD1140B7344b32f063dEe486",
+            (Network::Arbitrum, CompoundContract::CTokenv3, Asset::USDC) => "0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf",
+            (Network::Arbitrum, CompoundContract::CTokenv3, Asset::WETH) => "0x6f7D514bbD4aFf3BcD1140B7344b32f063dEe486",
           
             _ => anyhow::bail!("Contract address not configured yet"),
         };
@@ -78,31 +140,18 @@ impl Network{
     }
 
 
-
-
-
-
-
-
-
-
-    pub fn name(self:&Network)->Result<String>{
-        let name = match self{
-            Network::Ethereum=>"Ethereum",
-            Network::Arbitrum=>"Arbitrum",
-            Network::Base=>"Base",
-        };
-        Ok(name.to_string())
-    }
 }
 
 
-impl CompoundAsset{
-    pub fn name(self:&CompoundAsset)->Result<String>{
-        let name = match self {
-            CompoundAsset::USDC=>"USDC",
-            CompoundAsset::WETH=>"WETH"
-        };
-        Ok(name.to_string())
-    }
+
+#[derive(Debug, Clone, Copy)]
+pub enum AaveContract{
+    AaveProtocolDataProvider,
+    Pool
+}
+
+
+#[derive(Debug, Clone, Copy)]
+pub enum CompoundContract{
+    CTokenv3,
 }
